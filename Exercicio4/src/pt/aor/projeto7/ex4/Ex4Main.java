ac97e44493ed4aad074e419373ba4eb7110fd9ce
Edgar <Edgar@E-PC.home>
2015-07-07 22:25:42 +0100

    created exercice 4

---
+++ Exercicio4/src/pt/aor/projeto7/ex4/Ex4Main.java
@@ -0,0 +1,27 @@
+/*
+4. Consider a multi-threaded server and the following division of work: the master thread
+receives commands and the remaining worker threads execute these commands. To
+simplify, the purpose of the command is to compute the square root of a real number and
+print the result to the standard output (screen). You should write the program assuming
+that the master thread generates numbers at random, waiting some random time between
+two consecutive numbers. The master should use some form of queue to send the tasks to
+the workers (e.g., the ArrayBlockingQueue). You should do a print identifying the
+worker thread that is responsible for computing the result.
+ */
+
+package pt.aor.projeto7.ex4;
+
+/**
+ *
+ * @author Ed
+ */
+public class Ex4Main {
+
+    public static void main(String[] args){
+    
+    
+    }
+    
+    
+
+}
