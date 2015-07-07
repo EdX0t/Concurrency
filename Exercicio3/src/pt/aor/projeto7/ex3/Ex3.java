3d3ee4741210d762096a1084c6edcfa0d4c8ae77
Edgar <Edgar@E-PC.home>
2015-07-07 21:03:13 +0100

    exercicio 3 provisorio

--- Exercicio3/src/pt/aor/projeto7/ex3/Ex3.java
+++ Exercicio3/src/pt/aor/projeto7/ex3/Ex3.java
@@ -16,6 +16,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,32 +29,21 @@ public class Ex3 {
     private static final int numberOfThreads = 2;
     private static int numberOfDoubles;
     private static double[] listOfDoubles;
+    private static long zeroTime, splitTime, sequentialTime, threadedTime;
 
     public static void main(String[] args) {
-
+        zeroTime = System.nanoTime();
+        //creates the double array
         numberOfDoubles = Integer.valueOf(args[0]);
+        //generates random double values
         listOfDoubles = randomizeDoubles(numberOfDoubles);
-        //print average from array
-        System.out.println("Average: " + calculateAverage(listOfDoubles));
-
-        //initializa fixed thread pool with executor service
-        ExecutorService executors = Executors.newFixedThreadPool(numberOfThreads);
-
-        //initialize callables
-        Callable minCallable = new MinThread(listOfDoubles);
-        Callable maxCallable = new MaxThread(listOfDoubles);
-        //submit callables
-        Future minimum = executors.submit(minCallable);
-        Future maximum = executors.submit(maxCallable);
-
-        //print max and min from Future objects  
-        try {
-            System.out.println("Maximum: " + maximum.get());
-            System.out.println("Minimum: " + minimum.get());
-        } catch (InterruptedException | ExecutionException ex) {
-            Logger.getLogger(Ex3.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        splitTime = (System.nanoTime()-zeroTime)/1000;
+        //run threaded program
+        runThreadProgram();
+        //run sequential program
+        runSequentialProgram();
+        //write the time conclusions
+        writeConclusions();
     }
 
     private static double[] randomizeDoubles(int numberOfDoubles) {
@@ -75,5 +65,65 @@ public class Ex3 {
             soma += listOfDoubles[i];
         }
         return (soma / listOfDoubles.length);
+    }
+
+    private static void runThreadProgram() {
+        long threadStartTime = System.nanoTime();
+        //calculate and print average from array
+        System.out.println("Average: " + calculateAverage(listOfDoubles));
+
+        //initialize fixed thread pool with executor service
+        ExecutorService executors = Executors.newFixedThreadPool(numberOfThreads);
+
+        //initialize callables
+        Callable minCallable = new MinThread(listOfDoubles);
+        Callable maxCallable = new MaxThread(listOfDoubles);
+        //submit callables
+        Future minimum = executors.submit(minCallable);
+        Future maximum = executors.submit(maxCallable);
+
+        //print max and min from Future objects  
+        try {
+            System.out.println("Maximum: " + maximum.get());
+            System.out.println("Minimum: " + minimum.get());
+        } catch (InterruptedException | ExecutionException ex) {
+            Logger.getLogger(Ex3.class.getName()).log(Level.SEVERE, "Error getting minimum or maximum values.");
+        }
+        executors.shutdown();
+        threadedTime = (System.nanoTime() - threadStartTime)/1000;
+    
+    }
+
+    private static void runSequentialProgram() {
+        long sequentialStartTime = System.nanoTime();
+        //calculate and print average from array
+        System.out.println("Average: " + calculateAverage(listOfDoubles));
+        //calculate minimum
+        double min = listOfDoubles[0];
+        for (int i = 1; i < listOfDoubles.length; i++) {
+            if (listOfDoubles[i] < min) {
+                min = listOfDoubles[i];
+            }
+        }
+        System.out.println("Minimum: " + min);
+        //calculate maximum
+        double max = listOfDoubles[0];
+        for (int i = 1; i < listOfDoubles.length; i++) {
+            if (listOfDoubles[i] > max) {
+                max = listOfDoubles[i];
+            }
+        }
+        System.out.println("Minimum: " + max);
+
+        sequentialTime = (System.nanoTime() - sequentialStartTime)/1000;
+    }
+
+    private static void writeConclusions() {
+       System.out.println("Building the array time [microseconds]: "+ splitTime);
+       System.out.println("Threaded execution time [microseconds]: " + threadedTime);
+       System.out.println("Sequential execution time [microseconds]: " + sequentialTime);
+       
+       //Threaded time gain percentage for number of doubles argument
+       System.out.println("Overall threaded time gain % : "+(threadedTime/sequentialTime)*100);
     }
 }
