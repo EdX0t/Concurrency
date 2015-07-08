223c2c9c83d9079180951eea2ecd3e55f341d023
Edgar <Edgar@E-PC.home>
2015-07-08 16:21:25 +0100

    exercise 4 provisional updated sleep in master thread

---
+++ Exercicio4/src/pt/aor/projeto7/ex4/WorkerThread.java
@@ -0,0 +1,32 @@
+package pt.aor.projeto7.ex4;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ed
+ */
+public class WorkerThread implements Runnable{
+    
+   private final ArrayBlockingQueue<Double> arrayQueue;
+   
+    public WorkerThread(ArrayBlockingQueue<Double> queue){
+        this.arrayQueue = queue;
+    }
+
+    @Override
+    public void run() {
+       try {
+           Double value = arrayQueue.take();
+           System.out.println("Thread number "+Thread.currentThread().getId()+" calculated sqrt of "+value);
+           System.out.println("Result: "+Math.sqrt(value));
+                  
+       } catch (InterruptedException ex) {
+           Logger.getLogger(WorkerThread.class.getName()).log(Level.SEVERE, "Could not get the value from queue.");
+       }
+        
+    }
+
+}
