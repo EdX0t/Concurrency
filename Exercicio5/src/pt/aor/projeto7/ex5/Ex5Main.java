d9043bac06664259a6dea6b261264ff42e44257f
Edgar <Edgar@E-PC.home>
2015-07-08 19:40:48 +0100

    formated code, added suppress warning(sleep in thread)

---
+++ Exercicio5/src/pt/aor/projeto7/ex5/Ex5Main.java
@@ -0,0 +1,26 @@
+/*
+ *Write a program that creates two threads. During the execution of the program both
+threads should write either “ping” or “pong” to the screen, alternately, as follows:
+ping
+pong
+ping
+pong
+...
+Use the Exchanger class to synchronize the threads.
+ */
+package pt.aor.projeto7.ex5;
+
+/**
+ *
+ * @author Edgar
+ */
+public class Ex5Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
