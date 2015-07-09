0447f93c1f297322842a7e39b3a207b196024dcd
EdX0t <edgardg@sapo.pt>
2015-07-09 19:37:05 +0100

    update supress warns

---
+++ Exercicio8/src/pt/aor/projeto7/ex8/Exercicio8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.aor.projeto7.ex8;
+
+/**
+ *
+ * @author Edgar
+ */
+public class Exercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
